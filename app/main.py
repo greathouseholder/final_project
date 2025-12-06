683423bea92fc56959a58f86847f364f911ec783
greathouseholder <alkosmynin@gmail.com>
2025-12-06 20:37:07 +0300

    Minor main.py changes

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 import uvicorn
 
 from fastapi import FastAPI
-from src.api.v1.routers import router
+from src.api.v1.routers import routers
 
 
 app = FastAPI(
     title="RAG Telegram Bot for Lawyers API"
 )
 
-app.include_router(router)
+app.include_router(routers)
 
 
 @app.get("/health")
@@ -18,4 +18,4 @@ async def health_check():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
