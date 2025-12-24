8cb78a5cf35caefb75f6ba19c708fe13947c0414
JoelHalpert <dugaevdm@gmail.com>
2025-12-24 21:35:53 +0300

    убраны ненужные бд

--- app/src/shared/config.py
+++ app/src/shared/config.py
@@ -23,19 +23,6 @@ class Config:
     def qdrant_url(self) -> str:
         return f"http://{self.QDRANT_HOST}:{self.QDRANT_PORT}"
 
-    # POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "postgres")
-    # POSTGRES_PORT: int = int(os.getenv("POSTGRES_PORT", "5432"))
-    # POSTGRES_USER: str = os.getenv("POSTGRES_USER", "app")
-    # POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "")
-    # POSTGRES_DB: str = os.getenv("POSTGRES_DB", "rag_db")
-
-    # @property
-    # def postgres_dsn(self) -> str:
-    #     return (
-    #         f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
-    #         f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-    #     )
-
     ALLOW_ORIGINS: str = os.getenv("ALLOW_ORIGINS", "*")
     ALLOW_CREDENTIALS: str = os.getenv("ALLOW_CREDENTIALS", "true")
     ALLOW_METHODS: str = os.getenv("ALLOW_METHODS", "*")
