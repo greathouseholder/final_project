d8d37a6eab60b15f072779cc20d8e3c6a9a3d750
JoelHalpert <dugaevdm@gmail.com>
2025-12-24 19:55:48 +0300

    почти 55% тестов

--- app/src/infra/adapters/embeddings/__init__.py
+++ app/src/infra/adapters/embeddings/__init__.py
@@ -1,7 +1,4 @@
 from .interface import EmbedderInterface
-from .bert import BertEncoder
-from .bge import BGEEmbedder
-from .multilingual_e5 import MultilingualE5Embedder
 from .frida import FridaEmbedder
 
-__all__ = ["EmbedderInterface", "BertEncoder", "BGEEmbedder", "MultilingualE5Embedder", "Frida"]
+__all__ = ["EmbedderInterface", "FridaEmbedder"]
